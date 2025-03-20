5ca528c4250651472ec15d9a9b195bc6c591a10b
gilsu44145942 <asdf441467@gmail.com>
2025-03-20 10:06:15 +0900

    03/20

---
+++ artgallery/src/main/java/com/dw/artgallery/model/Drawing.java
@@ -0,0 +1,15 @@
+package com.dw.artgallery.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@Entity
+@Table(name="드로잉")
+public class Drawing {
+}
---
+++ artgallery/src/main/java/com/dw/artgallery/model/Purchase.java
@@ -0,0 +1,15 @@
+package com.dw.artgallery.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@Entity
+@Table(name="굿즈_구매내역")
+public class Purchase {
+}
