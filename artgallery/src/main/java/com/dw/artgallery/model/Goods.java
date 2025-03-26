2771cd4ac43c62e393e6c4d5ffdce35ee753bd13
RYU185 <fence365@naver.com>
2025-03-26 18:33:35 +0900

    Update Goods.java

--- artgallery/src/main/java/com/dw/artgallery/model/Goods.java
+++ artgallery/src/main/java/com/dw/artgallery/model/Goods.java
@@ -3,6 +3,8 @@ package com.dw.artgallery.model;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.List;
+
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
@@ -20,7 +22,7 @@ public class Goods {
     private String name;
 
     @Column(name="img_url",nullable = false)
-    private String imgUrl;
+    private List<String> imgUrlList;
 
     @Column(name = "description",nullable = false)
     private String description;
