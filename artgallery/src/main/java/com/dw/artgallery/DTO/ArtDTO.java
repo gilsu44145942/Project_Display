86c2aba5393c7180ecd11802c44f4075f75755a3
sungwoo1231 <tjddn159159@naver.com>
2025-04-08 18:26:00 +0900

    Update ArtDTO.java

--- artgallery/src/main/java/com/dw/artgallery/DTO/ArtDTO.java
+++ artgallery/src/main/java/com/dw/artgallery/DTO/ArtDTO.java
@@ -1,4 +1,29 @@
 package com.dw.artgallery.DTO;
 
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class ArtDTO {
+    private Long id;
+    private String title;
+    private String imgUrl;
+    private String description;
+    private LocalDate completionDate;
+    private LocalDate uploadDate;
+    private String artistName;  // Artist의 이름만 포함
+
+    public ArtDTO(Long id, String title, String imgUrl, String description, LocalDate completionDate, LocalDate uploadDate) {
+        this.id = id;
+        this.title = title;
+        this.imgUrl = imgUrl;
+        this.description = description;
+        this.completionDate = completionDate;
+        this.uploadDate = uploadDate;
+    }
 }
